6067232624ccba8c3cc6dce2243dc136625b7383
santiago-elustondo <elus2012@gmail.com>
2019-01-30 02:30:41 -0500

    h

--- 1-dev-server/src/components/App.js
+++ 1-dev-server/src/components/App.js
@@ -22,55 +22,55 @@ const styles = {
   },
 }
 
-class App extends React.PureComponent {
+export const App = withRouter(withStyles(styles)(
+  class extends React.PureComponent {
 
-  state = {
-    authState: 'LOADING',
-    open: false,
-  }
+    state = {
+      authState: 'LOADING',
+      open: false,
+    }
 
-  componentDidMount() {
-    thinker.subscribeToAuthState(state => this.setState({ authState: state }))
-  }
-  
-  render() {
-    const { history } = this.props
-    const { authState } = this.state
-
-    const currentPath = history.location.pathname
+    componentDidMount() {
+      thinker.subscribeToAuthState(state => this.setState({ authState: state }))
+    }
     
-    return (
-      <>
-        {
-          authState === 'LOADING' ? (
-            <LinearProgress />
-          ) : authState === 'LOGGED-OUT' && currentPath !== '/login' ? (
-            <Redirect to='/login'/>
-          ) : authState === 'LOGGED-IN' && currentPath === '/login' ? (
-            <Redirect to='/'/>
-          ) : authState === 'LOGGED-IN' && currentPath === '/' ? (
-            <Redirect to='/feed'/>
-          ) : (
-            <Switch>
-              <Route path='/login' component={AuthPage}/>
-              <AppFrame>
-                <Switch>
-                  <Route path='/feed' component={FeedPage}/>
-                  <Route path='/user/:userId/thought/:thoughtId' render={
-                    ({ match }) => <ThoughtPage {...match.params} />
-                  }/>
-                  <Route path='/user/:userId' render={
-                    ({ match }) => <UserPage {...match.params} />
-                  }/>
-                  <Route path='/' render={() => <h1>404</h1>}/>
-                </Switch>
-              </AppFrame>
-            </Switch>
-          )
-        }
-      </>
-    )
-  }
-}
+    render() {
+      const { history } = this.props
+      const { authState } = this.state
 
-export default withRouter(withStyles(styles)(App))
+      const currentPath = history.location.pathname
+      
+      return (
+        <>
+          {
+            authState === 'LOADING' ? (
+              <LinearProgress />
+            ) : authState === 'LOGGED-OUT' && currentPath !== '/login' ? (
+              <Redirect to='/login'/>
+            ) : authState === 'LOGGED-IN' && currentPath === '/login' ? (
+              <Redirect to='/'/>
+            ) : authState === 'LOGGED-IN' && currentPath === '/' ? (
+              <Redirect to='/feed'/>
+            ) : (
+              <Switch>
+                <Route path='/login' component={AuthPage}/>
+                <AppFrame>
+                  <Switch>
+                    <Route path='/feed' component={FeedPage}/>
+                    <Route path='/user/:userId/thought/:thoughtId' render={
+                      ({ match }) => <ThoughtPage {...match.params} />
+                    }/>
+                    <Route path='/user/:userId' render={
+                      ({ match }) => <UserPage {...match.params} />
+                    }/>
+                    <Route path='/' render={() => <h1>404</h1>}/>
+                  </Switch>
+                </AppFrame>
+              </Switch>
+            )
+          }
+        </>
+      )
+    }
+  }
+))
